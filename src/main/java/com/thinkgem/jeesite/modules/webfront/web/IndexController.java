76e5cc597aa9227fb88479ff76542d630d7475c7
xiang <xiang@xiang-PC>
2016-01-06 08:34:25 +0800

    前段登录修正

--- src/main/java/com/thinkgem/jeesite/modules/webfront/web/IndexController.java
+++ src/main/java/com/thinkgem/jeesite/modules/webfront/web/IndexController.java
@@ -9,7 +9,9 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.shiro.authz.UnauthorizedException;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.util.ByteSource;
 import org.apache.shiro.web.util.WebUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -25,15 +27,19 @@ import com.thinkgem.jeesite.common.config.Global;
 import com.thinkgem.jeesite.common.security.shiro.session.SessionDAO;
 import com.thinkgem.jeesite.common.servlet.ValidateCodeServlet;
 import com.thinkgem.jeesite.common.utils.CacheUtils;
+import com.thinkgem.jeesite.common.utils.Encodes;
 import com.thinkgem.jeesite.common.utils.IdGen;
 import com.thinkgem.jeesite.common.utils.StringUtils;
 import com.thinkgem.jeesite.common.web.BaseController;
 import com.thinkgem.jeesite.modules.cms.entity.Category;
 import com.thinkgem.jeesite.modules.cms.entity.Site;
 import com.thinkgem.jeesite.modules.cms.utils.CmsUtils;
+import com.thinkgem.jeesite.modules.sys.entity.User;
+import com.thinkgem.jeesite.modules.sys.security.FormAuthenticationFilter;
 import com.thinkgem.jeesite.modules.sys.security.SystemAuthorizingRealm.Principal;
-import com.thinkgem.jeesite.modules.sys.security.WebFormAuthenticationFilter;
+import com.thinkgem.jeesite.modules.sys.service.SystemService;
 import com.thinkgem.jeesite.modules.sys.utils.UserUtils;
+import com.thinkgem.jeesite.modules.sys.web.LoginController;
 import com.thinkgem.jeesite.modules.webfront.service.IndexService;
 
 /**
@@ -50,6 +56,8 @@ public class IndexController extends BaseController {
 	private IndexService indexService;
 	@Autowired
 	private SessionDAO sessionDAO;
+	@Autowired
+	private SystemService systemService;
 
 	/**
 	 * 网站首页
@@ -113,43 +121,59 @@ public class IndexController extends BaseController {
 	@RequestMapping(value = "login", method = RequestMethod.POST)
 	public String login(HttpServletRequest request, HttpServletResponse response, Model model) {
 		String targetURL = WebUtils.getCleanParam(request, "targetURL");
+		String username = WebUtils.getCleanParam(request, "username");
+		String password = WebUtils.getCleanParam(request, "password");
+		String captcha = WebUtils.getCleanParam(request, ValidateCodeServlet.VALIDATE_CODE);// 验证码
+		boolean isMobile = WebUtils.isTrue(request, FormAuthenticationFilter.DEFAULT_MOBILE_PARAM);
+		String message = "";
+
 		Principal principal = UserUtils.getPrincipal();
 
 		// 如果已经登录，则跳转到管理首页
 		if (principal != null) {
 			return "redirect:" + indexPath;
 		}
+		try {
+			// 校验登录验证码
+			if (LoginController.isValidateCodeLogin(username, false, false)) {
+				Session session = UserUtils.getSession();
+				String code = (String) session.getAttribute(ValidateCodeServlet.VALIDATE_CODE);
+				if (StringUtils.isBlank(captcha) || !captcha.toUpperCase().equals(code)) {
+					throw new Exception("验证码验证错误！");
+				}
+			}
 
-		String username = WebUtils.getCleanParam(request, WebFormAuthenticationFilter.DEFAULT_USERNAME_PARAM);
-		boolean rememberMe = WebUtils.isTrue(request, WebFormAuthenticationFilter.DEFAULT_REMEMBER_ME_PARAM);
-		boolean mobile = WebUtils.isTrue(request, WebFormAuthenticationFilter.DEFAULT_MOBILE_PARAM);
-		String exception = (String) request.getAttribute(WebFormAuthenticationFilter.DEFAULT_ERROR_KEY_ATTRIBUTE_NAME);
-		String message = (String) request.getAttribute(WebFormAuthenticationFilter.DEFAULT_MESSAGE_PARAM);
-
-		if (StringUtils.isBlank(message) || StringUtils.equals(message, "null")) {
-			message = "用户或密码错误, 请重试.";
-		}
-
-		model.addAttribute(WebFormAuthenticationFilter.DEFAULT_USERNAME_PARAM, username);
-		model.addAttribute(WebFormAuthenticationFilter.DEFAULT_REMEMBER_ME_PARAM, rememberMe);
-		model.addAttribute(WebFormAuthenticationFilter.DEFAULT_MOBILE_PARAM, mobile);
-		model.addAttribute(WebFormAuthenticationFilter.DEFAULT_ERROR_KEY_ATTRIBUTE_NAME, exception);
-		model.addAttribute(WebFormAuthenticationFilter.DEFAULT_MESSAGE_PARAM, message);
-
-		if (logger.isDebugEnabled()) {
-			logger.debug("login fail, active session size: {}, message: {}, exception: {}", sessionDAO.getActiveSessions(false).size(), message, exception);
-		}
-
-		// 非授权异常，登录失败，验证码加1。
-		if (!UnauthorizedException.class.getName().equals(exception)) {
+			// 校验用户名密码
+			User user = systemService.getUserByLoginName(username);
+			if (user != null) {
+				if (Global.NO.equals(user.getLoginFlag())) {
+					throw new Exception("该已帐号禁止登录!");
+				}
+				if(!SystemService.validatePassword(password, user.getPassword())){
+					throw new Exception("用户名密码错误！");
+				}
+			}
+			if (StringUtils.isBlank(message) || StringUtils.equals(message, "null")) {
+				message = "用户或密码错误, 请重试.";
+			}
+		} catch (Exception e) {
+			message = e.getLocalizedMessage();
+			// 非授权异常，登录失败，验证码加1。
 			model.addAttribute("isValidateCodeLogin", isValidateCodeLogin(username, true, false));
+			if (logger.isDebugEnabled()) {
+				logger.debug("login fail, active session size: {}, message: {}, exception: {}", sessionDAO.getActiveSessions(false).size(), message,e);
+			}
 		}
+
+		model.addAttribute("username", username);
+		model.addAttribute(FormAuthenticationFilter.DEFAULT_MOBILE_PARAM, isMobile);
+		model.addAttribute("message", message);
 
 		// 验证失败清空验证码
 		request.getSession().setAttribute(ValidateCodeServlet.VALIDATE_CODE, IdGen.uuid());
 
 		// 如果是手机登录，则返回JSON字符串
-		if (mobile) {
+		if (isMobile) {
 			return renderString(response, model);
 		}
 		return targetURL;
